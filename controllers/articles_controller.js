816ac126b90bcc6838d30151909a24c914ff123e
Laurent MARCHAND <e_lau@hotmail.fr>
2017-02-02 13:59:06 +0100

    Affichage Row Articles en JS

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,5 +1,5 @@
 
-function s() {
+function addArticlesRow() {
      $('.articles').load('../views/articles_row.html', function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                $('.articles_row').load('../views/left_article_row.html', function(responseTxt, statusTxt, xhr){
@@ -30,122 +30,9 @@ function s() {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
           }
      });
-
-
-     // $('.lastArticles')
-     // .append(
-     //     $("<div/>").attr("class", "col-xs-12 col-sm-6 img-padding imageDiv")
-     //     .append(
-     //         $("<div/>").attr("class", "view view-first")
-     //         .append(
-     //             $("<a/>").attr("href", "article.external_link")
-     //             .append(
-     //                 $("<img/>").attr("class", "img-responsive").attr("src", "../imgs/art1.jpeg"),
-     //                 $("<div/>").attr("class", "mask")
-     //                 .append(
-     //                     $("<p/>").text("article.desc"),
-     //                     $("<span/>").attr("class", "info").text("En savoir plus")
-     //                 ),
-     //                 $("<h3/>").attr("class", "title").text("article.title")
-     //
-     //             )
-     //         )
-     //     )
-     // );
-
-
-
-
-
-    // <!-- <div class="row articlesPlacer rowAdjust">
-    //     <div class="col-xs-1"></div>
-    //     <div class="col-xs-10">
-    //         <div class="row az">
-    //         </div>
-    //     </div>
-    //     <div class="col-xs-1"></div>
-    // </div> -->
-
-
-//     $(function() {
-//         $('.lastArticles').load('../views/left_article_row.html').attr("id", "rowNum1");
-//     });
-//     let rowbigDiv = document.querySelector("#rowNum1 .bigDiv");
-// //
-// console.log(rowbigDiv);
-//     rowbigDiv.append(
-//         $("<img/>").attr("class", "img-responsive").attr("src", "../imgs/art1.jpeg")
-//     );
-
-}
-
-
-
-function articleBigThumbnail(article) {
-
-    // $(function() {
-    //     $('.az').load('../views/article_view.html');
-    // });
-
-
-
-    $(".az").html("");
-    $(".az")
-    .append(
-        $("<div/>").attr("class", "col-xs-12 col-sm-6 img-padding imageDiv")
-        .append(
-            $("<div/>").attr("class", "view view-first")
-            .append(
-                $("<a/>").attr("href", article.external_link)
-                .append(
-                    $("<img/>").attr("class", "img-responsive").attr("src", "../imgs/art1.jpeg"),
-                    $("<div/>").attr("class", "mask")
-                    .append(
-                        $("<p/>").text(article.desc),
-                        $("<span/>").attr("class", "info").text("En savoir plus")
-                    ),
-                    $("<h3/>").attr("class", "title").text(article.title)
-
-                )
-            )
-        )
-    );
-}
-function articleLargegThumbnail(article) {
-    $(".az").html("");
-    let thumbnail = "trtr";
-    $(".az")
-    .append(
-        $("<div/>").attr("class", "col-xs-12 col-sm-6 img-padding imageDiv")
-        .append(
-            $("<div/>").attr("class", "view view-first")
-            .append(
-                $("<a/>").attr("href", article.external_link)
-                .append(
-                    $("<img/>").attr("class", "img-responsive").attr("src", "../imgs/art1.jpeg"),
-                    $("<div/>").attr("class", "mask")
-                    .append(
-                        $("<p/>").text(article.desc),
-                        $("<span/>").attr("class", "info").text("En savoir plus")
-                    ),
-                    $("<h3/>").attr("class", "title").text(article.title)
-
-                )
-            )
-        )
-    );
 }
 
 $(document).ready(function() {
-    // OK ############################################
-    // $.getJSON('../json/articles.json', function (data) {
-    //     for(item of data) {
-    //      //    console.log(item);
-    //         let article = new Article(item);
-    //         articleBigThumbnail(article);
-    //     }
-    // });
-
     $.ajax({
         url: '../json/articles.json',
         dataType: 'json',
