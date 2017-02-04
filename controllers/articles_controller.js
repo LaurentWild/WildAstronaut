9a2fd33eb5e871a85b7e86f9414f124eafa481f6
Laurent MARCHAND <e_lau@hotmail.fr>
2017-02-04 14:09:48 +0100

    Block articles optimisation 1

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,24 +1,11 @@
 
-function loadFirstArticleView(article) {
-     $('#firstArticle').load('../views/big_article_view.html', function(responseTxt, statusTxt, xhr){
+function loadFirstArticleView(article, divId, file, descStyle, imageSize) {
+     $(divId).load(file, function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // FIRST DIV
                let firstDiv = $(this).context.children[0];
-               firstDiv.setAttribute("id", "divArticle" + article.id);
-               firstDiv.className += " v1";
-               // IMAGE
-               let img = firstDiv.querySelector("img");
-               img.setAttribute("id", "img" + article.id);
-               img.setAttribute("src", "../imgs/" + article.img);
-               img.setAttribute("id", "img" + article.id);
-               // TITLE
-               let title = firstDiv.querySelector("h3");
-               title.setAttribute("id", "title" + article.id);
-               title.innerHTML = article.title;
-               // DESC
-               let desc = firstDiv.querySelector("p");
-               desc.setAttribute("id", "desc" + article.id);
-               desc.innerHTML = article.desc;
+               // DISPLAY ARTICLE
+               displayArticle(firstDiv, article, descStyle, imageSize);
            }
            if(statusTxt == "error") {
                alert("Error: " + xhr.status + ": " + xhr.statusText);
@@ -26,26 +13,13 @@ function loadFirstArticleView(article) {
      });
 }
 
-function loadSecondArticleView(article) {
-     $('#secondArticle').load('../views/large_article_view.html', function(responseTxt, statusTxt, xhr){
+function loadSecondArticleView(article, divId, file, descStyle, imageSize) {
+     $(divId).load(file, function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // FIRST DIV
                let firstDiv = $(this).context.children[0];
-               firstDiv.setAttribute("id", "divArticle" + article.id);
-               firstDiv.className += " v1";
-               // IMAGE
-               let img = firstDiv.querySelector("img");
-               img.setAttribute("id", "img" + article.id);
-               img.setAttribute("src", "../imgs/" + article.img);
-               img.setAttribute("id", "img" + article.id);
-               // TITLE
-               let title = firstDiv.querySelector("h3");
-               title.setAttribute("id", "title" + article.id);
-               title.innerHTML = article.title;
-               // DESC
-               let desc = firstDiv.querySelector("p");
-               desc.setAttribute("id", "desc" + article.id);
-               desc.innerHTML = article.desc;
+               // DISPLAY ARTICLE
+               displayArticle(firstDiv, article, descStyle, imageSize);
           }
           if(statusTxt == "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
@@ -53,26 +27,13 @@ function loadSecondArticleView(article) {
      });
 }
 
-function loadThirdArticleView(article) {
-     $('#thirdArticle').load('../views/big_article_view.html', function(responseTxt, statusTxt, xhr){
+function loadThirdArticleView(article, divId, file, descStyle, imageSize) {
+     $(divId).load(file, function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // FIRST DIV
                let firstDiv = $(this).context.children[0];
-               firstDiv.setAttribute("id", "divArticle" + article.id);
-               firstDiv.className += " v1";
-               // IMAGE
-               let img = firstDiv.querySelector("img");
-               img.setAttribute("id", "img" + article.id);
-               img.setAttribute("src", "../imgs/" + article.img);
-               img.setAttribute("id", "img" + article.id);
-               // TITLE
-               let title = firstDiv.querySelector("h3");
-               title.setAttribute("id", "title" + article.id);
-               title.innerHTML = article.title;
-               // DESC
-               let desc = firstDiv.querySelector("p");
-               desc.setAttribute("id", "desc" + article.id);
-               desc.innerHTML = article.desc;
+               // DISPLAY ARTICLE
+               displayArticle(firstDiv, article, descStyle, imageSize);
           }
           if(statusTxt == "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
@@ -80,31 +41,39 @@ function loadThirdArticleView(article) {
      });
 }
 
-function loadFourthArticleView(article) {
-     $('#fourthArticle').load('../views/big_article_view.html', function(responseTxt, statusTxt, xhr){
+function loadFourthArticleView(article, divId, file, descStyle, imageSize) {
+     $(divId).load(file, function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // FIRST DIV
                let firstDiv = $(this).context.children[0];
-               firstDiv.setAttribute("id", "divArticle" + article.id);
-               firstDiv.className += " v1";
-               // IMAGE
-               let img = firstDiv.querySelector("img");
-               img.setAttribute("id", "img" + article.id);
-               img.setAttribute("src", "../imgs/" + article.img);
-               img.setAttribute("id", "img" + article.id);
-               // TITLE
-               let title = firstDiv.querySelector("h3");
-               title.setAttribute("id", "title" + article.id);
-               title.innerHTML = article.title;
-               // DESC
-               let desc = firstDiv.querySelector("p");
-               desc.setAttribute("id", "desc" + article.id);
-               desc.innerHTML = article.desc;
+               // DISPLAY ARTICLE
+               displayArticle(firstDiv, article, descStyle, imageSize);
           }
           if(statusTxt == "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
           }
      });
+}
+function displayArticle(firstDiv, article, descStyle, imageSize) {
+     // FIRST DIV
+     firstDiv.setAttribute("id", "divArticle" + article.id);
+     firstDiv.className += " " + descStyle;
+     // IMAGE
+     let img = firstDiv.querySelector("img");
+     img.setAttribute("id", "img" + article.id);
+     img.setAttribute("src", "../imgs/" + article.img);
+     img.setAttribute("id", "img" + article.id);
+     if(imageSize === "rectangle") {
+
+     }
+     // TITLE
+     let title = firstDiv.querySelector("h3");
+     title.setAttribute("id", "title" + article.id);
+     title.innerHTML = article.title;
+     // DESC
+     let desc = firstDiv.querySelector("p");
+     desc.setAttribute("id", "desc" + article.id);
+     desc.innerHTML = article.desc;
 }
 
 function loadLeftArticleRow(articles) {
@@ -112,19 +81,32 @@ function loadLeftArticleRow(articles) {
      // console.log(articles[0].title);
      $('#articlesRow').load('../views/left_article_row.html', function(responseTxt, statusTxt, xhr){
           if(statusTxt === "success") {
-               loadFirstArticleView(articles[0]);
-                    // console.log("gfghfhfghfghfghe");
-               loadSecondArticleView(articles[1]);
-               loadThirdArticleView(articles[2]);
-               loadFourthArticleView(articles[3]);
+               let divId = "#firstArticle";
+               let file = '../views/big_article_view.html';
+               let descStyle = "v1";
+               let imageSize = "square";
+               loadFirstArticleView(articles[0], divId, file, descStyle, imageSize);
+               divId = "#secondArticle";
+               file = '../views/large_article_view.html';
+               descStyle = "v2";
+               imageSize = "rectangle";
+               loadSecondArticleView(articles[1], divId, file, descStyle, imageSize);
+               divId = "#thirdArticle";
+               file = '../views/big_article_view.html';
+               descStyle = "v4";
+               imageSize = "square";
+               loadThirdArticleView(articles[2], divId, file, descStyle, imageSize);
+               divId = "#fourthArticle";
+               file = '../views/big_article_view.html';
+               descStyle = "v4";
+               imageSize = "square";
+               loadFourthArticleView(articles[3], divId, file, descStyle, imageSize);
           }
           if(statusTxt === "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
           }
      });
 }
-
-
 
 function addArticlesRow(articles) {
      $('.articles').load('../views/articles_row.html', function(responseTxt, statusTxt, xhr){
