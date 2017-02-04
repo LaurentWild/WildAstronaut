4d993ac4f87b201f8219e27d583086d14417b4b4
Laurent MARCHAND <e_lau@hotmail.fr>
2017-02-04 15:36:44 +0100

    Block articles optimisation 2

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -1,25 +1,10 @@
-
-function loadFirstArticleView(article, divId, file, descStyle, imageSize) {
+function loadArticleView(article, divId, file, descStyle) {
      $(divId).load(file, function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // FIRST DIV
                let firstDiv = $(this).context.children[0];
                // DISPLAY ARTICLE
-               displayArticle(firstDiv, article, descStyle, imageSize);
-           }
-           if(statusTxt == "error") {
-               alert("Error: " + xhr.status + ": " + xhr.statusText);
-           }
-     });
-}
-
-function loadSecondArticleView(article, divId, file, descStyle, imageSize) {
-     $(divId).load(file, function(responseTxt, statusTxt, xhr){
-          if(statusTxt == "success") {
-               // FIRST DIV
-               let firstDiv = $(this).context.children[0];
-               // DISPLAY ARTICLE
-               displayArticle(firstDiv, article, descStyle, imageSize);
+               displayArticle(firstDiv, article, descStyle);
           }
           if(statusTxt == "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
@@ -27,41 +12,14 @@ function loadSecondArticleView(article, divId, file, descStyle, imageSize) {
      });
 }
 
-function loadThirdArticleView(article, divId, file, descStyle, imageSize) {
-     $(divId).load(file, function(responseTxt, statusTxt, xhr){
-          if(statusTxt == "success") {
-               // FIRST DIV
-               let firstDiv = $(this).context.children[0];
-               // DISPLAY ARTICLE
-               displayArticle(firstDiv, article, descStyle, imageSize);
-          }
-          if(statusTxt == "error") {
-              alert("Error: " + xhr.status + ": " + xhr.statusText);
-          }
-     });
-}
-
-function loadFourthArticleView(article, divId, file, descStyle, imageSize) {
-     $(divId).load(file, function(responseTxt, statusTxt, xhr){
-          if(statusTxt == "success") {
-               // FIRST DIV
-               let firstDiv = $(this).context.children[0];
-               // DISPLAY ARTICLE
-               displayArticle(firstDiv, article, descStyle, imageSize);
-          }
-          if(statusTxt == "error") {
-              alert("Error: " + xhr.status + ": " + xhr.statusText);
-          }
-     });
-}
-function displayArticle(firstDiv, article, descStyle, imageSize) {
+function displayArticle(firstDiv, article, descStyle) {
      // FIRST DIV
      firstDiv.setAttribute("id", "divArticle" + article.id);
      firstDiv.className += " " + descStyle;
      // IMAGE
      let img = firstDiv.querySelector("img");
      let image = article.img;
-     if(imageSize === "rectangle") {
+     if(descStyle === "v2") {
           let imgTab = article.img.split("_");
           image = imgTab[0] + "_r.jpeg";
      }
@@ -79,30 +37,14 @@ function displayArticle(firstDiv, article, descStyle, imageSize) {
 }
 
 function loadLeftArticleRow(articles) {
-
-     // console.log(articles[0].title);
      $('#articlesRow').load('../views/left_article_row.html', function(responseTxt, statusTxt, xhr){
           if(statusTxt === "success") {
-               let divId = "#firstArticle";
-               let file = '../views/big_article_view.html';
-               let descStyle = "v1";
-               let imageSize = "square";
-               loadFirstArticleView(articles[0], divId, file, descStyle, imageSize);
-               divId = "#secondArticle";
-               file = '../views/large_article_view.html';
-               descStyle = "v2";
-               imageSize = "rectangle";
-               loadSecondArticleView(articles[1], divId, file, descStyle, imageSize);
-               divId = "#thirdArticle";
-               file = '../views/big_article_view.html';
-               descStyle = "v4";
-               imageSize = "square";
-               loadThirdArticleView(articles[2], divId, file, descStyle, imageSize);
-               divId = "#fourthArticle";
-               file = '../views/big_article_view.html';
-               descStyle = "v4";
-               imageSize = "square";
-               loadFourthArticleView(articles[3], divId, file, descStyle, imageSize);
+               let file = '../views/article_view.html';
+               let divIds = ["#firstArticle", "#secondArticle", "#thirdArticle", "#fourthArticle"];
+               let descStyles = ["v1", "v2","v4", "v4"];
+               for(let i in divIds) {
+                    loadArticleView(articles[i], divIds[i], file, descStyles[i]);
+               }
           }
           if(statusTxt === "error") {
               alert("Error: " + xhr.status + ": " + xhr.statusText);
@@ -111,7 +53,7 @@ function loadLeftArticleRow(articles) {
 }
 
 function addArticlesRow(articles) {
-     $('.articles').load('../views/articles_row.html', function(responseTxt, statusTxt, xhr){
+     $('#articles').load('../views/articles_row.html', function(responseTxt, statusTxt, xhr){
           if(statusTxt == "success") {
                // console.log("102");
                loadLeftArticleRow(articles);
@@ -136,6 +78,9 @@ function addArticlesBlock(articles) {
 
 $(document).ready(function() {
      let articles = [];
+     // CHECK ARTICLES DIV
+     let divArticles = document.querySelector("#articles");
+     // console.log(divArticles.className);
      $.ajax({
           url: '../json/articles.json',
           dataType: 'json',
@@ -145,9 +90,10 @@ $(document).ready(function() {
                for(item of data) {
                     i++;
                     let article = new Article(item);
-
                     articles.push(article);
-                    if(i === 4) break;
+                    // if(divArticles.className === "a4") {
+                         if(i === 4) break;
+                    // }
                }
                // console.log("ART =>" + articles);
                addArticlesBlock(articles);
